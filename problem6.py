bb420f03cb25110805e31481e6bb673eee3b18aa
Peter Jönsson <peter.joensson@gmail.com>
2011-04-16 13:52:13 +0200

    solved problem 6.

--- problem6.py
+++ problem6.py
@@ -1,12 +1,26 @@
 #!/usr/bin/env python
 
-def sum_sqaures(num):
+def sum_squares(num):
     sum = 0
-    i = num
+    i = 0
 
     while(i<=num):
-        sum = sum + (i*i)
+        sum += (i*i)
+        i += 1
 
     return sum
 
-print sum_sqaures(10)
+def square_sum(num):
+
+    s = sum(range(1, num+1))
+    prod = s * s
+
+    return prod
+
+num = 100
+
+s_sq = sum_squares(num)
+sq_s = square_sum(num)
+
+print str(sq_s) + " - " + str(s_sq) + " = ", 
+print sq_s - s_sq

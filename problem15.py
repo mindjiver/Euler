f50aa9848ec0dc4d939b2524b36688c7e0aee086
Peter Jönsson <peter.joensson@gmail.com>
2011-04-17 02:27:56 +0200

    first version.

---
+++ problem15.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+# the choice is binary, so (2 choose 1). For a 20x20 grid there are 21
+# choices to be made.
+#
+# (n over k ) => n! / k!(n - k)
+# 2 over 1  => 2*1 / 1*(2 - 1) = 2 / 1 = 2
+#
+# So, 2 * 21 = 42

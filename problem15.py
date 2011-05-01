6c8949550e7dc6d7d04103ce20965972c7e18ae0
Peter Jönsson <peter.joensson@gmail.com>
2011-05-01 19:21:45 +0200

    Solved 15, had to look up the correct n over k formula, tack kimmo!

--- problem15.py
+++ problem15.py
@@ -1,2 +1,19 @@
 #!/usr/bin/env python
 
+from math import factorial
+
+def permutations(n, k):
+    #
+    # n choose m = n! / k!(n-k)!
+    #
+    return factorial(n) / (factorial(k) * factorial(n - k))
+    
+#
+# n = 2
+#
+# We need to cover 4 blocks to reach the destination, for this we need
+# to walk 2 east and 2 south in any combination.
+#
+# 
+n = 20
+print permutations(n*2, n)

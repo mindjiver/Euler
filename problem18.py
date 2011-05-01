c02e79aa7f06ddf535f50c970330dc4666b35f3e
Peter Jönsson <peter.joensson@gmail.com>
2011-05-01 17:19:17 +0200

    Solved 18 by brute force, just to try out some graph stuff.

--- problem18.py
+++ problem18.py
@@ -1,17 +1,60 @@
 #!/usr/bin/env python
 
-75
-95 64
-17 47 82
-18 35 87 10
-20 04 82 47 65
-19 01 23 75 03 34
-88 02 77 73 07 63 67
-99 65 04 28 06 16 70 92
-41 41 26 56 83 40 80 70 33
-41 48 72 33 47 32 37 16 94 29
-53 71 44 65 25 43 91 52 97 51 14
-70 11 33 28 77 73 17 78 39 68 17 57
-91 71 52 38 17 14 91 43 58 50 27 29 48
-63 66 04 68 89 53 67 30 73 16 69 87 40 31
-04 62 98 27 23 09 70 98 73 93 38 53 60 04 23
+class Node():
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+def node_creator(lists, x, y):
+
+    if lists[x][y] == None:
+        return None
+
+    return Node(lists[x][y],
+                node_creator(lists, x+1, y),
+                node_creator(lists, x+1, y+1)
+                )
+
+def graph_brute_force(node, path):
+
+    (l, v, r) = (node.left, node.value, node.right)
+
+    path = path + [v]
+    
+    if l == None or r == None:
+        return path
+
+    #left first
+    return graph_brute_force(l, path) + graph_brute_force(r, path)
+
+g = []
+n = 0
+
+with open("problem18_input.txt") as f:
+    data = f.readlines();
+    n = len(data)
+    for line in data:
+        l = line.strip().split(" ")
+        l = [int(i) for i in l]
+        g.append(l)
+    # append a list with None to simply parsing
+    g.append([None] * 2 * len(g[-1]))
+
+g = node_creator(g, 0, 0)
+routes = graph_brute_force(g, [])
+
+i = 0
+max_sum = 0
+max_path = []
+
+while(i+n < len(routes)):
+    path = routes[i:i+n]
+    path_sum = sum(path)
+    if path_sum > max_sum:
+        max_sum = path_sum
+        max_path = path
+    i += n
+
+print max_path
+print max_sum

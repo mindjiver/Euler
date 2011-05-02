55d1144f057297c1bf245000776cc833c25d6f7e
Peter Jönsson <peter.joensson@gmail.com>
2011-05-02 17:45:16 +0200

    Started on a brute force solution, not working at all.

--- problem23.py
+++ problem23.py
@@ -3,3 +3,33 @@
 # < deficient
 # = perfect
 # > abundant
+
+def divisors(n):
+
+    divs = []
+    for i in range(1, n):
+        if n % i == 0:
+            divs.append(i)
+            
+    return divs
+
+n  = 28123
+
+abundant = []
+
+for i in range(1, n+1):
+    div_sum = sum(divisors(i))
+
+    if div_sum > i:
+        abundant.append(i)
+
+for i in range(1, n+1):
+    print "Checking " + str(i)
+    a = False
+    for j in abundant:
+        if i - j in abundant:
+            a = True
+            break
+
+    if a == False:
+        print str(i) + " is not a sum of two abundant #."

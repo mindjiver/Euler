0791f3a1da11cad99ebcfb5f7d43f476309f33aa
Peter Jönsson <peter.jonsson@klarna.com>
2013-12-16 16:57:03 +0100

    Fix bug, thanks @lndbrg

--- elixir/problem01.exs
+++ elixir/problem01.exs
@@ -4,7 +4,7 @@
 # look at.
 #
 
-range = 1..1000
+range = 1..1000-1
 
 s = Stream.filter(range, fn(x) -> rem(x, 3) == 0 or rem(x, 5) == 0 end)
 {_, sum} = Enum.map_reduce(s, 0, fn(x, acc) -> { x, x + acc } end)
